346e9d6546f60b9ee4ea90ff00a9f86b120fb071
Даниил Соловьев <sdgroup14@gmail.com>
2025-09-01 22:38:44 +0300

    fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,20 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    // соответствует вашему client-route 'blog/:slug'
+    path: 'blog',
+    renderMode: RenderMode.Server,
+  },
+   {
+    // соответствует вашему client-route 'blog/:slug'
+    path: 'blog/:slug',
+    renderMode: RenderMode.Server,
+  },
+
+  {
     path: '**',
-    renderMode: RenderMode.Server
-  }
+    renderMode: RenderMode.Prerender
+  },
+
+
 ];
